f338bd006392c800aa4116c74a1e600d10faa5a3
T-tssxuan <2270360114@qq.com>
2017-07-30 00:35:12 +0800

    [new]

--- log_analysis/get_loss.sh
+++ log_analysis/get_loss.sh
@@ -1,5 +1,5 @@
 awk '{ if (NR > 10) print $0}' ../nohup.out > tmp
 awk '{ if (NR % 3 == 2) print $0}' tmp > tmp1
 awk 'BEGIN{FS=","} {print $2}' tmp1 > tmp2
-awk 'BEGIN{FS=": "} {print $2}' tmp2 > data
+awk 'BEGIN{FS=": "} {print $2}' tmp2 > data/loss
 rm tmp*
