4452340d684941ae86ae622e3dc85611f1a2b28f
T-tssxuan <2270360114@qq.com>
2017-07-30 23:18:35 +0800

    [new] add fasttext word

--- zhihu/models/fasttext_word/train.py
+++ zhihu/models/fasttext_word/train.py
@@ -2,9 +2,9 @@ from ..fasttext.fasttext import FastText
 from ...utils.tools import Tools
 import os, sys
 
-log = Tools.get_logger('fasttext char')
+log = Tools.get_logger('fasttext word')
 
-class FastTextChar:
+class FastTextWord:
     def __init__(self, epoch=100, thread=30, dim=128):
         cur_path = os.path.dirname(os.path.realpath(__file__))
         if not os.path.exists(cur_path + '/model'):
@@ -12,7 +12,7 @@ class FastTextChar:
         if not os.path.exists(cur_path + '/data'):
             os.mkdir(cur_path + '/data')
         model = 'model_{}_{}'.format(epoch, dim)
-        self.obj = FastText(cur_path, 'char', epoch, thread, dim, model)
+        self.obj = FastText(cur_path, 'word', epoch, thread, dim, model)
 
     def train(self):
         log.info('begin train')
@@ -30,7 +30,7 @@ class FastTextChar:
         log.info('end eval')
 
 if __name__ == '__main__':
-    ftc = FastTextChar(epoch=5, thread=12)
+    ftc = FastTextWord(epoch=5, thread=12)
     ftc.train()
     ftc.test()
     ftc.eval()
